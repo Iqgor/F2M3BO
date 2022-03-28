3133543fc5009c81b37e08edde382bacae566cc6
Kietmandjes <kietmandjes@gmail.com>
2022-03-28 13:30:03 +0200

    kiet changes

--- js/main.js
+++ js/main.js
@@ -66,3 +66,43 @@ let data = fetch("https://api.openweathermap.org/data/2.5/weather?q=netherlands&
     temp.innerText = realData.main.temp + " °C"
 })
 
+//weerbericht vandaag//
+let weather = {
+   "apiKey" : "8a490c6651725451fa03123bd0d7b472",
+   fetchWeather: function(city) {
+       fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+       + city 
+       + "&units=metric&appid="
+       + this.apiKey
+       )    
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+   },
+   displayWeather: function(data) {
+    const { name } = data;
+    const { icon , description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name,icon,description,temp,humidity,speed)
+    document.querySelector(".city").innerHTML = "Weer in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png"
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Vochtigheid: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind snelheid: " + speed + "km/h";
+   },
+   search: function () {
+       this.fetchWeather(document.querySelector(".search-bar").value);
+   }
+};
+
+document
+  .querySelector(".search button")
+  .addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    } 
+})
